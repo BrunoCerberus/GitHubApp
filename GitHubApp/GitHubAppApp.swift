8d103ad56a55cea871fe9a2d2560784620449729
Bruno Lopes <bruno.guitarpro@gmail.com>
2023-05-28 14:21:36 -0300

    first commit

---
+++ GitHubApp/GitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAppApp.swift
+//  GitHubApp
+//
+//  Created by bruno on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
