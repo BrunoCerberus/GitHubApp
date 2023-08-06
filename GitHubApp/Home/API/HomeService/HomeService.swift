748904dfcfe00fb93aebb5c01777dd2c0aee55ed
Bruno Lopes <bruno.guitarpro@gmail.com>
2023-08-06 01:55:33 -0300

    Service should provide a publisher with an error too

--- GitHubApp/Home/API/HomeService/HomeService.swift
+++ GitHubApp/Home/API/HomeService/HomeService.swift
@@ -8,9 +8,8 @@
 import Combine
 
 final class HomeService: APIRequest {
-    func fetchMovies() -> AnyPublisher<MoviesResponse, Never> {
+    func fetchMovies() -> AnyPublisher<MoviesResponse, Error> {
         self.fetchRequest(target: HomeAPI.fetchMovies, dataType: MoviesResponse.self)
-            .replaceError(with: [])
             .eraseToAnyPublisher()
     }
 }
