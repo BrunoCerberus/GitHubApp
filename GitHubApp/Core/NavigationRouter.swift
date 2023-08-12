58d642a8e52f14e34bccb00d0628f7644d525b51
Bruno Lopes <bruno.guitarpro@gmail.com>
2023-08-12 18:32:51 -0300

    Improve NavigationRouter protocol

--- GitHubApp/Core/NavigationRouter.swift
+++ GitHubApp/Core/NavigationRouter.swift
@@ -5,9 +5,12 @@
 //  Created by bruno on 29/05/23.
 //
 
+import UIKit
+
 public protocol NavigationRouter {
     // Navigation event that will be triggered by View
     associatedtype NavigationEventType
 
+    var navigation: UINavigationController? { get set }
     func route(navigationEvent: NavigationEventType)
 }
