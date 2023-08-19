d784b9af732a8e225e57a7769168f361fa766a67
Bruno Lopes <bruno.guitarpro@gmail.com>
2023-08-18 23:42:52 -0300

    Set properly build configuration to separate environments

--- GitHubApp/Configs/APIKeys.swift
+++ GitHubApp/Configs/APIKeys.swift
@@ -8,23 +8,12 @@
 import Foundation
 
 struct APIKeys {
-    #if DEBUG
     static let theMovieAPIKey: String = {
-        guard let path = Bundle.main.path(forResource: "Dev", ofType: "xcconfig"),
-              let config = NSDictionary(contentsOfFile: path),
-              let apiKey = config["API_KEY"] as? String else {
-            fatalError("Unable to read development API key from configuration file.")
-        }
-        return apiKey
-    }()
-    #else
-    static let theMovieAPIKey: String = {
-        guard let path = Bundle.main.path(forResource: "Prod", ofType: "xcconfig"),
+        guard let path = Bundle.main.path(forResource: ProcessInfo.processInfo.environment["API_ENVIRONMENT"], ofType: "xcconfig"),
               let config = NSDictionary(contentsOfFile: path),
               let apiKey = config["API_KEY"] as? String else {
             fatalError("Unable to read production API key from configuration file.")
         }
         return apiKey
     }()
-    #endif
 }
