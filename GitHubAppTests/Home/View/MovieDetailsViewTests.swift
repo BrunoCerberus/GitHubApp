5b0657d724dbc017b898553bd18e802c45c453a8
Bruno Lopes <bruno.guitarpro@gmail.com>
2023-08-22 21:37:38 -0300

    Add MovieDetailsViewTests

---
+++ GitHubAppTests/Home/View/MovieDetailsViewTests.swift
@@ -0,0 +1,40 @@
+//
+//  MovieDetailsViewTests.swift
+//  GitHubAppTests
+//
+//  Created by bruno on 22/08/23.
+//
+
+import Combine
+import SnapshotTesting
+import SwiftUI
+import XCTest
+
+@testable import GitHubApp
+
+final class MovieDetailsViewTests: XCTestCase {
+    let movie = Movie(id: 346698,
+                      title: "Barbie",
+                      overview: "Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. However, when they get a chance to go to the real world, they soon discover the joys and perils of living among humans.",
+                      posterPath: "")
+    var mockService: MockHomeService!
+    var viewModel: MovieDetailsViewModel!
+    var view: MovieDetailsView!
+    
+    override func setUp() {
+        super.setUp()
+        
+        mockService = MockHomeService()
+        viewModel = MovieDetailsViewModel(movie: movie, service: mockService)
+        view = MovieDetailsView(viewModel: viewModel)
+    }
+    
+    func testView() {
+        let vc = view.wrappedViewController
+        let nav = UINavigationController(rootViewController: vc)
+
+        viewModel.fetchData()
+        
+        assertSnapshot(matching: nav, as: .wait(for: 0.3, on: .image(on: .iPhoneSe)))
+    }
+}
